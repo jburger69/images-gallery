a52340c6853a1da7eaba1cb94ed18c9407285a54
Joshua Burger <joshberger6969@outlook.com>
2021-10-19 20:13:41 -0400

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
+const UNSPLASH = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   }
 
 
