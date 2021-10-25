1487efcaadf70f266a19fe8468a0be42db089c8c
Joshua Burger <joshberger6969@outlook.com>
2021-10-25 15:49:35 -0400

    docker-compose for frontend and api

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -17,7 +17,7 @@ const Search = ({word, setWord, handleSubmit}) => {
                 />
               </Col>
               <Col>
-                <Button variant="primary" type="submit">Primary</Button>
+                <Button variant="primary" type="submit">Search</Button>
               </Col>
             </Row>
           </Form>
