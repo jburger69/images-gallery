58a5ee0d2fce4cd282faae5eedf550247c019ce9
Joshua Burger <joshberger6969@outlook.com>
2021-10-28 18:51:58 -0400

    display authors name and portfolio if givin

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Nav } from 'react-bootstrap';
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name;
+  const authorPortfolioURL = image.user?.portfolio_url;
+
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img
@@ -16,6 +20,10 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
         <Button variant="primary" onClick={() => deleteImage(image.id)} >Delete</Button>{' '}
         {!image.saved && (<Button variant="secondary" onClick={() => saveImage(image.id)}>Save</Button>)}
       </Card.Body>
+      <Card.Footer className='text-center text-muted'>
+        {authorPortfolioURL && <Nav.Link href={authorPortfolioURL} target="_blank">{authorName}</Nav.Link>}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
